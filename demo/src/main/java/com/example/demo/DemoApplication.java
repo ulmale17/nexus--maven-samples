f93013dc817a5413890cb890eadfa6bbd7fd3f4b
ulmale17 <149786729+ulmale17@users.noreply.github.com>
2023-11-10 14:29:26 +0530

    Update DemoApplication.java

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -11,3 +11,9 @@ public class DemoApplication {
 	}
 
 }
+public class HelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+   }
+}
